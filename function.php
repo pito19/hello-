f2c625ee6507dbf0608bc23f6fd279b01382b045
kotb TAOUI <em01@hotmail.fr>
2019-04-23 11:50:19 +0200

    start

---
+++ function.php
@@ -0,0 +1,9 @@
+<?php 
+function getAllAnnonces(){
+    $data =[
+        "annonce1" => ["voiture", 1000]
+    ];
+    return $data;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+require "function.php";
+
+$resultat = getAllAnnonces();
+?>
+
+
+ <ul>
+ 	<li><?php echo $resultat["annonce1"][0] ?></li>
+ 	<li><?php echo $resultat["annonce1"][1] ?></li>
+ </ul>
