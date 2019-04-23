dd30309d24bbd2d15326ec63a206c95248a15009
kotb TAOUI <em01@hotmail.fr>
2019-04-23 12:13:39 +0200

    modification terminée

--- index.php
+++ index.php
@@ -9,3 +9,9 @@ $resultat = getAllAnnonces();
  	<li><?php echo $resultat["annonce1"][0] ?></li>
  	<li><?php echo $resultat["annonce1"][1] ?></li>
  </ul>
+ 
+<hr>
+<ul>
+ 	<li><?php echo $resultat["annonce2"][0] ?></li>
+ 	<li><?php echo $resultat["annonce2"][1] ?></li>
+ </ul>
