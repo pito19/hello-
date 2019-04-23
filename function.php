79126230a370e70c027d645c62ea8d5a4bee939f
kotb TAOUI <em01@hotmail.fr>
2019-04-23 11:59:11 +0200

    ajout d'une nouvelle annonce

--- function.php
+++ function.php
@@ -1,7 +1,8 @@
 <?php 
 function getAllAnnonces(){
     $data =[
-        "annonce1" => ["voiture", 1000]
+        "annonce1" => ["voiture", 1000], 
+        "annonce2" => ["meuble", 55] 
     ];
     return $data;
 }
